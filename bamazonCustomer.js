55943e9b19e4bc2a96469711c3cd017c51223bf4
Jacob Lewis <jacoblewis@Jacobs-MacBook-Pro.local>
2019-10-15 15:24:37 -0700

    created bamazonCustomer.js. installed required packages (inquirer, mysql, and cli-table). connected to mysql.

---
+++ bamazonCustomer.js
@@ -0,0 +1,19 @@
+// require variables
+var table = require("cli-table");
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// connection to mysql
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Redsox4093!",
+    database: "bamazon"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected as id: " + connection.threadId);
+    // startPrompt();
+});
